a079a3a6f1a2ceeda71963c4a2e8f03d0c85e49c
adeshkothavade <adesh.kothavade@bluepineapple.io>
2023-06-02 09:47:55 +0530

    fetching quotes from remote json

--- src/Quotes.js
+++ src/Quotes.js
@@ -1,6 +1,10 @@
 import React from "react";
 import "./App.css";
-import { quoteData } from "./data";
+import { BlobServiceClient } from "@azure/storage-blob"
+import { useState } from 'react';
+import { Buffer } from 'buffer';
+
+window.Buffer = Buffer;
 
 const getRandomInt = (min=0, max) => {
     min = Math.ceil(min);
@@ -8,15 +12,55 @@ const getRandomInt = (min=0, max) => {
     return Math.floor(Math.random() * (max - min) + min);
 }
 
+const getQuotes = async () => {
+    const account = process.env.REACT_APP_STORAGE_ACCOUNT;
+    const sas = process.env.REACT_APP_STORAGE_SAS;
+    console.log('account: %s', account)
+    console.log('sas: %s', sas)
+    let connString = 'https://' + account + '.blob.core.windows.net?' + sas;
+    const blobServiceClient = new BlobServiceClient(connString);
+    const container = process.env.REACT_APP_STORAGE_CONTAINER;
+    const fileName = process.env.REACT_APP_QUOTES_FILENAME
+
+    const containerClient = blobServiceClient.getContainerClient(container);
+    const blobClient = containerClient.getBlockBlobClient(fileName);
+
+    const downloadBlockBlobResponse = await blobClient.download(0);
+    const downloadedContent = await streamToString(downloadBlockBlobResponse.readableStreamBody);
+
+    const quoteData = JSON.parse(downloadedContent)
+
+    return quoteData.quotes
+};
+
+const streamToString = async (readableStream) => {
+    const chunks = [];
+    for await (const chunk of readableStream) {
+    chunks.push(chunk);
+    }
+    return new TextDecoder().decode(new Uint8Array(chunks.flat()));
+};
+
+
 export const Quotes = () => {
-    let randomIndex = getRandomInt(0, quoteData.length)
-    let selectedQuote = quoteData[randomIndex];
+    const [quoteDetail, setQuoteDetail] = useState(undefined);
+    const [showDiv, setShowDiv] = useState(false);
+
+    const getQuote = async () => {
+        var quoteData = getQuotes();
+        let randomIndex = getRandomInt(0, quoteData.length)
+        let selectedQuote = quoteData[randomIndex];
+        setQuoteDetail(selectedQuote)
+        setShowDiv(true);
+    }
+
     return (
         <>
             <h1> Get a quote here !! </h1>
-            <div className="quote-container">
-                {selectedQuote.quote + "  @" + selectedQuote.author}
-            </div>
+            <button onClick= {getQuote}>Get Quote</button>
+            {showDiv && <div className="quote-container">
+                {quoteDetail?.quote + "  @" + quoteDetail?.author}
+            </div>}
         </>
     );
 };
